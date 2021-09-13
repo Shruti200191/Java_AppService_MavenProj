749972d1c4f2accc43d3041e3755720778e237c2
Shruti200191 <78497721+Shruti200191@users.noreply.github.com>
2021-09-14 01:40:20 +0530

    Update Status.java

--- src/main/java/com/miguno/Status.java
+++ src/main/java/com/miguno/Status.java
@@ -11,7 +11,7 @@ public class Status {
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   public String statusInformation() {
-//     return "{\"status\": \"idle\"}\n";
-    return "App Server is up and Running";
+    return "{\"status\": \"idle\"}\n";
+//     return "App Server is up and Running";
   }
 }
